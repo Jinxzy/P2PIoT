96e029a820b4312b0fd4277d709edacf032c555a
hermetico <yojmarinv@gmail.com>
2016-03-04 16:23:45 +0100

    structure milestone 4

---
+++ test/milestone4/RestTest.java
@@ -0,0 +1,30 @@
+package milestone4;
+
+import milestone4.Node;
+
+public class RestTest {
+	public static void main(String[] args) {
+		int num_nodes = 5, multiplier = 2, bootstrap_port = 49960;
+		String bootstrap_host = "localhost";
+
+
+		Node[] nodes = new Node[num_nodes];
+		final Node root = new Node(bootstrap_host, bootstrap_port);
+		root.join();
+		nodes[0] = root;
+		for(int i = 1; i < num_nodes; i++)
+		{
+			int port = bootstrap_port + i;
+			nodes[i] = new Node(bootstrap_host, port);
+			nodes[i].join(root.getIp(), root.getPort());
+		}
+		root.findResponsibleNode();
+		
+		for(int i = 0; i < num_nodes; i++) {
+			System.out.println("-----------------------");
+			System.out.println("NodeID: " + nodes[i].getID());
+			nodes[i].printPhotonInfo();
+			System.out.println("-----------------------");
+		}
+	}
+}
