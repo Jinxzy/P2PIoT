262cccdb6b67d106a2388033c5a3282dc5fe5001
Nicolai <nicolai_nibe@hotmail.com>
2016-03-07 12:56:19 +0100

    - Missing classes i forgot to commit

---
+++ src/milestone4/DataListWrapper.java
@@ -0,0 +1,15 @@
+package milestone4;
+
+import java.util.ArrayList;
+
+public class DataListWrapper {
+	private ArrayList<PhotonData> list;
+	
+	public ArrayList<PhotonData> getList() {
+	    return list;
+	}
+	
+	public void setList(ArrayList<PhotonData> list) {
+	    this.list = list;
+	}   
+}
